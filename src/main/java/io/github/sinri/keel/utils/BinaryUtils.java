0ff0fa7bd34973fc034db8c87d25fa6cd0fc14c6
Sinri Edogawa <e.joshua.s.e@gmail.com>
2025-11-07 19:08:40 +0800

    Add utility classes `BinaryUtils`, `StringUtils`, and `StackUtils`, along with `Base32` encoder/decoder implementation for enhanced string, binary, and stack trace operations.
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

---
+++ src/main/java/io/github/sinri/keel/utils/BinaryUtils.java
@@ -0,0 +1,136 @@
+package io.github.sinri.keel.utils;
+
+import io.github.sinri.keel.utils.encryption.base32.Base32;
+import io.vertx.core.buffer.Buffer;
+
+import javax.annotation.Nonnull;
+import java.util.Base64;
+
+public class BinaryUtils {
+    final static char[] HEX_DIGITS_LOWER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
+    final static char[] HEX_DIGITS_UPPER = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'};
+
+    private BinaryUtils() {
+    }
+
+    private static @Nonnull String encodeHexWithDigits(final char[] HEX_DIGITS, @Nonnull Buffer buffer, int since, int length) {
+        StringBuilder hex = new StringBuilder();
+        for (int i = since; i < since + length; i++) {
+            hex
+                    .append(HEX_DIGITS[(0xF0 & buffer.getByte(i)) >>> 4])
+                    .append(HEX_DIGITS[0x0F & buffer.getByte(i)])
+            ;
+        }
+        return hex.toString();
+    }
+
+    /**
+     * @param data an array of byte
+     * @return expression with hex using lower digits as string
+     * @since 1.11
+     */
+    public static String encodeHexWithLowerDigits(@Nonnull final byte[] data) {
+        return encodeHexWithLowerDigits(Buffer.buffer(data));
+    }
+
+    /**
+     * @param buffer an instance of Buffer defined in Vertx
+     * @return expression with hex using lower digits as string
+     * @since 1.11
+     */
+    public static @Nonnull String encodeHexWithLowerDigits(@Nonnull Buffer buffer) {
+        return encodeHexWithLowerDigits(buffer, 0, buffer.length());
+    }
+
+    /**
+     * @param buffer an instance of Buffer defined in Vertx
+     * @param since  the start index
+     * @param length the length
+     * @return expression of the substring with hex using lower digits as string
+     * @since 1.11
+     */
+    public static @Nonnull String encodeHexWithLowerDigits(@Nonnull Buffer buffer, int since, int length) {
+        return encodeHexWithDigits(HEX_DIGITS_LOWER, buffer, since, length);
+    }
+
+    /**
+     * @param data an array of bytes
+     * @return expression with hex using upper digits as string
+     * @since 1.11
+     */
+    public static @Nonnull String encodeHexWithUpperDigits(@Nonnull final byte[] data) {
+        return encodeHexWithUpperDigits(Buffer.buffer(data));
+    }
+
+    /**
+     * @param buffer an instance of Buffer defined in Vertx
+     * @return expression of the substring with hex using upper digits as string
+     * @since 1.11
+     */
+    public static @Nonnull String encodeHexWithUpperDigits(@Nonnull Buffer buffer) {
+        return encodeHexWithUpperDigits(buffer, 0, buffer.length());
+    }
+
+    /**
+     * @param buffer an instance of Buffer defined in Vertx
+     * @param since  the start index
+     * @param length the length
+     * @return expression of the substring with hex using upper digits as string
+     * @since 1.11
+     */
+    public static @Nonnull String encodeHexWithUpperDigits(@Nonnull Buffer buffer, int since, int length) {
+        return encodeHexWithDigits(HEX_DIGITS_UPPER, buffer, since, length);
+    }
+
+    /**
+     * @since 2.9.4
+     */
+    public static @Nonnull byte[] decodeWithBase64(@Nonnull byte[] bytes) {
+        return Base64.getDecoder().decode(bytes);
+    }
+
+    /**
+     * @since 2.9.4
+     */
+    @Nonnull
+    public static byte[] encodeWithBase64(@Nonnull byte[] bytes) {
+        return Base64.getEncoder().encode(bytes);
+    }
+
+    /**
+     * @since 2.9.4
+     */
+    @Nonnull
+    public static String encodeWithBase64ToString(@Nonnull byte[] bytes) {
+        return new String(encodeWithBase64(bytes));
+    }
+
+    /**
+     * @since 2.9.4
+     */
+    public static @Nonnull byte[] encodeWithBase32(@Nonnull byte[] bytes) {
+        return encodeWithBase32ToString(bytes).getBytes();
+    }
+
+    /**
+     * @since 2.9.4
+     */
+    @Nonnull
+    public static String encodeWithBase32ToString(@Nonnull byte[] bytes) {
+        return Base32.encode(bytes);
+    }
+
+    /**
+     * @since 2.9.4
+     */
+    public static @Nonnull byte[] decodeWithBase32(@Nonnull byte[] bytes) {
+        return Base32.decode(new String(bytes));
+    }
+
+    /**
+     * @since 2.9.4
+     */
+    public static @Nonnull String decodeWithBase32ToString(@Nonnull byte[] bytes) {
+        return new String(decodeWithBase32(bytes));
+    }
+}
