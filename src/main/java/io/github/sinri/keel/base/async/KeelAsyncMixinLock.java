b702c8cbf26bf98f425654fe30c0bce0b522a0ab
sinri <e.joshua.s.e@gmail.com>
2026-02-24 13:41:25 +0800

    feat(async): enhance `KeelAsyncMixinLock` for improved error handling and flexibility
    - Extended `asyncCallExclusively` method to support custom error handling on lock acquisition failure.
    - Introduced `LockAcquireFailedException` for more descriptive handling of lock issues.
    - Updated Javadoc for clarity and added `@since 5.0.2` annotations.
    
    Signed-off-by: sinri <e.joshua.s.e@gmail.com>

--- src/main/java/io/github/sinri/keel/base/async/KeelAsyncMixinLock.java
+++ src/main/java/io/github/sinri/keel/base/async/KeelAsyncMixinLock.java
@@ -3,6 +3,7 @@ package io.github.sinri.keel.base.async;
 import io.vertx.core.Future;
 import org.jspecify.annotations.NullMarked;
 
+import java.util.function.Function;
 import java.util.function.Supplier;
 
 
@@ -18,28 +19,75 @@ interface KeelAsyncMixinLock extends KeelAsyncMixinCore {
      * <p>
      * 尝试在制定时间限制内获取锁，并运行异步逻辑，执行完毕后释放锁。
      *
-     * @param <T>               异步逻辑的返回值类型
-     * @param lockName          锁名称
-     * @param waitTimeForLock   最长锁等待时间，以毫秒计
-     * @param exclusiveSupplier 需要独占运行的异步逻辑
+     * @param <T>                             异步逻辑的返回值类型
+     * @param lockName                        锁名称
+     * @param waitTimeForLock                 最长锁等待时间，以毫秒计
+     * @param exclusiveSupplier               需要独占运行的异步逻辑
+     * @param lockAcquireFailedHandleSupplier 锁获取失败时的处理函数，返回一个Future，用于替代锁获取失败的异常
      * @return 异步逻辑的结果；如果锁获取失败，则会异步返回相应失败。
+     * @since 5.0.2
      */
-    default <T> Future<T> asyncCallExclusively(String lockName, long waitTimeForLock,
-                                               Supplier<Future<T>> exclusiveSupplier) {
+    default <T> Future<T> asyncCallExclusively(
+            String lockName,
+            long waitTimeForLock,
+            Supplier<Future<T>> exclusiveSupplier,
+            Function<LockAcquireFailedException, Future<T>> lockAcquireFailedHandleSupplier
+    ) {
         return sharedData()
-                         .getLockWithTimeout(lockName, waitTimeForLock)
-                         .compose(lock -> Future.succeededFuture()
-                                                .compose(v -> exclusiveSupplier.get())
-                                                .andThen(ar -> lock.release()));
+                .getLockWithTimeout(lockName, waitTimeForLock)
+                .compose(
+                        lock -> Future.succeededFuture()
+                                      .compose(v -> exclusiveSupplier.get())
+                                      .andThen(ar -> lock.release()),
+                        throwable -> {
+                            LockAcquireFailedException lockAcquireFailedException = new LockAcquireFailedException(throwable.getMessage());
+                            return lockAcquireFailedHandleSupplier.apply(lockAcquireFailedException);
+                        }
+                );
     }
 
     /**
      * 在Vertx 的锁机制下，独占运行一段异步逻辑。
      * <p>
-     * 和 {@link KeelAsyncMixinLock#asyncCallExclusively(String, long, Supplier)} 逻辑一致，锁等待时间默认 1 秒。
+     * 和 {@link KeelAsyncMixinLock#asyncCallExclusively(String, long, Supplier, Function)} 逻辑一致，
+     * 获取锁异常不进行额外处理。
+     */
+    default <T> Future<T> asyncCallExclusively(
+            String lockName,
+            long waitTimeForLock,
+            Supplier<Future<T>> exclusiveSupplier
+    ) {
+        return asyncCallExclusively(
+                lockName,
+                waitTimeForLock,
+                exclusiveSupplier,
+                Future::failedFuture
+        );
+    }
+
+    /**
+     * 在Vertx 的锁机制下，独占运行一段异步逻辑。
+     * <p>
+     * 和 {@link KeelAsyncMixinLock#asyncCallExclusively(String, long, Supplier, Function)} 逻辑一致，锁等待时间默认 1 秒。
      */
     default <T> Future<T> asyncCallExclusively(String lockName,
                                                Supplier<Future<T>> exclusiveSupplier) {
-        return asyncCallExclusively(lockName, 1_000L, exclusiveSupplier);
+        return asyncCallExclusively(
+                lockName,
+                1_000L,
+                exclusiveSupplier,
+                Future::failedFuture
+        );
+    }
+
+    /**
+     * 锁获取失败异常
+     *
+     * @since 5.0.2
+     */
+    class LockAcquireFailedException extends RuntimeException {
+        public LockAcquireFailedException(String cause) {
+            super(cause);
+        }
     }
 }
